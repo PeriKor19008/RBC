b83711c72e285fa9230a6fe928307c6b08683819
PeriKor19008 <peri1908kor@gmail.com>
2025-10-29 19:24:41 +0200

    stable before adding perscentage based checkpoint selection

--- src/model/main.py
+++ src/model/main.py
@@ -8,20 +8,16 @@ def train_single_model():
     #placeholders
 
     #best with diferent loss func and weight decay
-    train_CNN(32,1, 0.001,
+    train_CNN(32,25, 0.001,
               [("conv", 4),("conv", 8),("conv", 16), ("conv", 32),("conv", 64), ("conv", 120),("conv", 240)], [128],True)
 
-    # train_CNN(32, 25, 0.001,
-    #           [("conv", 4), ("conv", 8), ("conv", 16), ("conv", 32), ("conv", 64), ("conv", 120), ("conv", 240)], [128],
-    #           True)
-    #
-    # train_CNN(32, 25, 0.001,
-    #           [("conv", 4), ("conv", 8), ("conv", 16), ("conv", 32), ("conv", 64), ("conv", 120), ("conv", 240)], [128],
-    #           True)
-    #
-    # train_CNN(32, 25, 0.001,
-    #           [("conv", 4), ("conv", 8), ("conv", 16), ("conv", 32), ("conv", 64), ("conv", 120), ("conv", 240)], [128],
-    #           True)
+    train_CNN(32, 25, 0.001,
+              [("conv", 4), ("conv", 8), ("conv", 16), ("conv", 32), ("conv", 64), ("conv", 120), ("conv", 240)], [128],
+              True)
+
+    train_CNN(32, 25, 0.001,
+              [("conv", 4), ("conv", 8), ("conv", 16), ("conv", 32), ("conv", 64), ("conv", 120), ("conv", 240)], [128],
+              True)
 
     #_, _, rd = run_autoencoder(32, 50, 0.0001, [1024, 512, 128], 64,True)
 
